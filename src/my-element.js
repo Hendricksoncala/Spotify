9079185b4166ecd5977801be4e553b50f5f91f4e
Hendricksoncala <156349949+Hendricksoncala@users.noreply.github.com>
2024-05-13 17:37:08 -0500

    Revert "feat:"
    This reverts commit d269a731215bf42e0f3d81a543f9944e05e3f8fd.

--- src/my-element.js
+++ src/my-element.js
@@ -13,9 +13,8 @@ class Myelement extends LitElement{
     this.plustime = "2:14"
     this.negativetime = " -1:15"
 
-    this.imagen = "/src/assets/metroboom.png"
 
-    this.dispositive = "Airpos Pro (Dave)"
+    this.imagen = "/src/assets/metroboom.png"
   }
 //CSS DEL OSOCOCOCOCOCOCOSOSOSOSOSOSOSOSOSOSOS
   static styles = css`
@@ -38,7 +37,7 @@ class Myelement extends LitElement{
     height:100vh;
     background: white;
     grid-template-columns:  1fr  ;
-    grid-template-rows: 0.3fr 3fr 0.5fr 0.5fr 0.5fr 0.5fr 0.3fr;   
+    grid-template-rows: 0.3fr 3fr 0.8fr 0.6fr 1fr 0.5fr 0.3fr;   
     grid-template-areas: 
     " titule "
     " container__image "
@@ -114,6 +113,7 @@ class Myelement extends LitElement{
   .line_song{
     // background: brown;
     display:flex;
+
     flex-direction: column;
     color: black
   
@@ -140,7 +140,7 @@ class Myelement extends LitElement{
     cursor: pointer; /* Cambiamos el cursor a una mano */
   }
   .line_song input{
-    width: 90%
+    width: 70%
 
   }
   .line_song .time_line{
@@ -152,14 +152,14 @@ class Myelement extends LitElement{
   /*PARTE DE CAMBIAR LA CANCION CHANGE_SONG----------------------*/ 
   .change_song{
     grid-area: change_song;
-    // background: orange;
+    background: orange;
     display:flex;
     justify-content: space-evenly;
 
   }
   .change_song img{
 
-
+    image: url('src/assets/play button.svg');
     background-size: contain;
     width: 1.5vw; /* ajusta el ancho y alto según sea necesario */
     height: 100%;
@@ -168,7 +168,7 @@ class Myelement extends LitElement{
 
   }
   .change_song .play{
-
+    image: url('src/assets/play button.svg');
     background-size: contain;
     width: 6vw; /* ajusta el ancho y alto según sea necesario */
     height: 100%;
@@ -183,46 +183,10 @@ class Myelement extends LitElement{
     grid-area: song_volumen;
     background: blue;
     display:flex;
-    justify-content: center;
-  
-  }
-  .song_volumen .volumen_line input[type="range"] {
-    background-color: gray; /* Color de fondo del input */
-    height: 5px; /* Altura del input */
-    width:50%;
-    border-radius: 2px; /* Redondeamos los bordes */
-    accent-color: #1DB954; /* Color del thumb (verde) */
+    justify-content: row;
+    flex-direction: space-between
 
   }
-
-  .song_volumen .volumen_line input[type="range"]::-webkit-slider-thumb {
-    -webkit-appearance: none; /* Quitamos la apariencia por defecto del thumb */
-    width: 20px; /* Ancho del thumb */
-    height: 20px; /* Alto del thumb */
-    border-radius: 50%; /* Redondeamos el thumb */
-    background-color: #00ff00; /* Color del thumb (verde) */
-    cursor: pointer; /* Cambiamos el cursor a una mano */
-  }
-  .volumen_line{
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    width:100%
-
-  }
-  .volumen_line input{
-    width: 80%
-
-  }
-  .song_volumen .aleatory{
-    background-size: contain;
-    width: 1.5vw; /* ajusta el ancho y alto según sea necesario */
-    height: 100%;
-    border: none;
-    cursor: pointer; /* cambia el cursor a una mano para indicar que es clickable */
-
-  }
-
 
   /*DISPOSITIVE--------------------------------------------------*/
   .dispositive{
@@ -273,27 +237,20 @@ class Myelement extends LitElement{
           </div>
         </div>
         <div class="change_song">
+
           <img src="src/assets/Group 45.svg" class="aleatory">
           <img src="src/assets/Group.svg"  class="left">
           <img src="src/assets/play button.svg" class="play">
           <img src="src/assets/Group right.svg" class="right">
           <img src="src/assets/Group 44.svg" class="rewind">
+
+
         </div>
-
         <div class="song_volumen">
-
-          <div class="volumen_line">
-            <img src="src/assets/low_sound.svg" class="aleatory">
-            <input type="range" name="slider" id="slide2">
-            <img src="src/assets/up_sound.svg" class="aleatory">
-          </div>
-
+          <img>
         </div>
         <div class="dispositive">
-          <div class="info_dispositive">
-            <img src="src/assets/Vector.svg" class="aleatory">
-            <p>${this.dispositive}</p>
-          </div>
+          <img>
         </div>
       </div>
         
