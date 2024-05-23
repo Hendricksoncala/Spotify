b5ef20ef77f07dcf8767ba786c10dc9541f45c34
Hendricksoncala <156349949+Hendricksoncala@users.noreply.github.com>
2024-05-23 09:13:33 -0500

    feat: :sparkles: agregando my-trick, y ajustando las importaciones

--- src/my-grid.js
+++ src/my-grid.js
@@ -1,23 +1,41 @@
 import { LitElement, html, css } from "lit";
-import { Myelement } from "./my-element.js";
-import { newMusic } from "./top-chart";
-import { myTrack } from "./playing-next";
-import { MyLeftBar } from "./segundoElement";
+
+import { MyLeftBar } from "./my-left-bar";
+customElements.define('my-left-bar', MyLeftBar);
+
+import { Cardnewmusic } from "./newMusic";
+
+import { myTrack } from "./my-track";
+customElements.define('my-track', myTrack);
+
+import { Reproductor } from "./reproductor";
+customElements.define('my-reproductor', Reproductor);
+
+import { TrackList } from "./my-track-list";
+customElements.define('my-trcklist', TrackList);
+
 
 
 class myGrid extends LitElement {
 
     static styles = css`
     
+    *{
+        margin: none;
+        padding: none;
+        box-sizing: border-box;
+
+    }
+
     .main{
         height: 100vh;
-        width: 100vw;
         display: grid;
         grid-template-columns: 80px 1fr 1.5fr 1fr;
         grid-template-rows: 1fr;
     }
     .main__aside{
-        background: #2B2D42;
+        height: 100vh;
+        background: #dfdfdf;
         padding: 10px;
         display: grid;
         grid-template-rows: 130px 1fr 130px
@@ -32,24 +50,51 @@ class myGrid extends LitElement {
         
     }
     .main__section1{
-        background: #EC9A29;
+        height: 100vh;
+        background: white;
+        box-shadow: 9px 0 20px -20px;
     }
+
+    .main__section1 h1{
+        font-size: 3em;
+        margin-left: 5%
+    }
+
+    .top__chart__title{
+        display: flex;
+        justify-content: space-between;
+        padding: 0 5%;
+    }
+
+    .youmaylike h2{
+        margin: 15px;
+    }
+
     .main__section2{
-        background: #EF233C;
+        height: 100vh;
+        z-index: -1
     }
-    .main__section2{
-        background: #35A7FF;
-    }
+
     .main__section3{
-        background: #E8E9ED;
+        height: 100vh;
+        overflow-y: scroll;
+        background: white;
+        box-shadow: 10px 10px 20px;
     }
+
+    .trcklist {
+        overflow-y: scroll;
+    }
+
+    .tracklistupper {
+        margin-left: 20px;
+    }
+
+
     @media (max-width: 849px){
         .main{
             grid-template-columns: 1fr;
             grid-template-rows: 1fr 80px;
-        }
-        .main__aside{
-            order: 1;
         }
         .aside__profile{
             display: none
@@ -60,7 +105,7 @@ class myGrid extends LitElement {
         .main__section1{
             display: none
         }
-        .main__section3{
+        .main__section2{
             display: none
         }
     }
@@ -70,26 +115,35 @@ class myGrid extends LitElement {
         return html`
         <main class="main">
             <aside class="main__aside">
-                <article class="aside__profile">
-                    <my-left-bar></my-left-bar>
-                </article>
-
-                <article class="aside__buttons">
-
-                </article>
-                <article class="aside__exit"></article>
+            <my-left-bar></my-left-bar>
             </aside>
+
             <section class="main__section1">
-                <h1 class="tituloh1">Discover new Music</h1>
-                <h2>Top-Chart</h2>
-                <new-music class="new-music"></new-music>
+                <h1>Discover<br>New music</h1>
+                    <div class="top__chart__title">
+                        <h2>Top-chart</h2><h2>Week</h2>
+                    </div>
+
+            <new-music></new-music>
+
+                <div class="youmaylike">
+                    <h2>You may Like</h2>
+                </div>
+
+            <my-track></my-track>
+            
             </section>
             <section class="main__section2">
-                <my-element></my-element>
+            <my-reproductor><my-reproductor>
+            
             </section>
             <section class="main__section3">
-                <h1 class="tituloh1">Track-list</h1>
-                <my-track></my-track>
+            <div class="tracklistupper"> 
+            <h1>Track List</h1>
+            <h2>Playing Next</h2>
+            <div>
+
+            <my-trcklist class="trcklist"></my-trcklist>
             </section>
         </main>
         `
@@ -97,7 +151,3 @@ class myGrid extends LitElement {
 }
 
 customElements.define("my-grid" , myGrid)
-customElements.define("my-element", Myelement)
-customElements.define("new-music", newMusic);
-customElements.define('my-track', myTrack);
-customElements.define('my-left-bar', MyLeftBar);
